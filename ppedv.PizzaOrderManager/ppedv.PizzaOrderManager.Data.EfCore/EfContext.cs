c9438250e646fa25a4fc0f7e5482d89c486f9b79
André <AndreR@ppedv.de>
2024-07-25 10:21:55 +0200

    EfCore ist da

---
+++ ppedv.PizzaOrderManager/ppedv.PizzaOrderManager.Data.EfCore/EfContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.PizzaOrderManager.Model;
+using System.Diagnostics;
+
+namespace ppedv.PizzaOrderManager.Data.EfCore
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+
+        string conString;
+
+        public EfContext(string conString)
+        {
+            this.conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(conString)
+                          .LogTo(msg => Debug.WriteLine(msg));
+        }
+
+    }
+}
