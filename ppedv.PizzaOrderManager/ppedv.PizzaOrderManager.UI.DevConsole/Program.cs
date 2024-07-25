364c1d2ab6cdda7d3937fdc3bc5d45d7c5d151e0
André <AndreR@ppedv.de>
2024-07-25 15:24:41 +0200

    Dependency Injection

--- ppedv.PizzaOrderManager/ppedv.PizzaOrderManager.UI.DevConsole/Program.cs
+++ ppedv.PizzaOrderManager/ppedv.PizzaOrderManager.UI.DevConsole/Program.cs
@@ -1,23 +1,43 @@
-﻿using ppedv.PizzaOrderManager.Data.EfCore;
+﻿using Autofac;
+using ppedv.PizzaOrderManager.Data.EfCore;
 using ppedv.PizzaOrderManager.Logic;
 using ppedv.PizzaOrderManager.Model.Contracts;
 using ppedv.PizzaOrderManager.Model.DomainModel;
+using System.Reflection;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 Console.WriteLine("🍕🍕🍕 Pizza Order Manager v0.1 🍕🍕🍕");
 
 string conString = "Server=(localdb)\\mssqllocaldb;Database=PizzaOrderManager_test;Trusted_Connection=true;";
-IRepository repo = new  EfContextIRepositoryAdapter(conString);
 
-var os = new OrderService(repo);
+
+//Di per Hand
+//IRepository repo = new EfContextIRepositoryAdapter(conString);
+//var os = new OrderService(repo);
+
+//Dependency Injection per Reflection
+//var path = @"C:\Users\Fred\source\repos\241404_Architekturen\ppedv.PizzaOrderManager\ppedv.PizzaOrderManager.Data.EfCore\bin\Debug\net8.0\ppedv.PizzaOrderManager.Data.EfCore.dll";
+//var ass = Assembly.LoadFrom(path);
+//var typeMitRepo = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IRepository)));
+//IRepository repo = (IRepository)Activator.CreateInstance(typeMitRepo,conString);
+//var os = new OrderService(repo);
+
+//Di per AutoFac
+var builder = new ContainerBuilder();
+builder.RegisterType<OrderService>();
+builder.Register(x => new EfContextIRepositoryAdapter(conString)).AsImplementedInterfaces().SingleInstance();
+var container = builder.Build();
+
+IRepository repo = container.Resolve<IRepository>();
+var os = container.Resolve<OrderService>();
 
 foreach (var adr in repo.GetAll<Address>())
 {
     Console.WriteLine($"{adr.Name1} {adr.Name2}");
-	foreach (var order in os.GetMyOrders(adr.Id))
-	{
+    foreach (var order in os.GetMyOrders(adr.Id))
+    {
         Console.WriteLine($"\t {order.OrderDate}");
-	}
+    }
 }
 
 Console.WriteLine("Ende");
