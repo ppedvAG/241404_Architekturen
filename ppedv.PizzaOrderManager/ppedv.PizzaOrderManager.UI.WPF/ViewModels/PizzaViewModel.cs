a0177564a651f56d777fa822e1c1fec16f4ffe5c
André <AndreR@ppedv.de>
2024-07-25 16:53:52 +0200

    WPF mit ein bisschen MVVM

---
+++ ppedv.PizzaOrderManager/ppedv.PizzaOrderManager.UI.WPF/ViewModels/PizzaViewModel.cs
@@ -0,0 +1,44 @@
+﻿using ppedv.PizzaOrderManager.Model.DomainModel;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace ppedv.PizzaOrderManager.UI.WPF.ViewModels
+{
+    public class PizzaViewModel
+    {
+        public ObservableCollection<Pizza> PizzaList { get; set; } = new ObservableCollection<Pizza>();
+        public Pizza SelectedPizza { get; set; }
+
+        public AddNewCommand AddNewCommand { get; set; }
+
+        public PizzaViewModel()
+        {
+            PizzaList.Add(new Pizza() { Name = "Salami", Toppings = [new Topping() { Name = "Käse" }, new Topping() { Name = "Salami" }] });
+            PizzaList.Add(new Pizza() { Name = "Hawai", Toppings = [new Topping() { Name = "Käse" }, new Topping() { Name = "Ananas" }, new Topping() { Name = "Schinken" }] });
+
+            AddNewCommand = new AddNewCommand(PizzaList);
+        }
+
+    }
+
+    public class AddNewCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public ObservableCollection<Pizza> PizzaList;
+        public AddNewCommand(ObservableCollection<Pizza> pizzaList)
+        {
+            PizzaList = pizzaList;
+        }
+
+        public void Execute(object? parameter)
+        {
+            PizzaList.Add(new Pizza() { Name = "NEU" });
+        }
+    }
+}
