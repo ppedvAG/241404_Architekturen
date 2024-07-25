7e3d702d31ce0f2576c179ff574d3faacf367712
André <AndreR@ppedv.de>
2024-07-25 13:31:43 +0200

    OrderService ist da

---
+++ ppedv.PizzaOrderManager/ppedv.PizzaOrderManager.Logic/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using ppedv.PizzaOrderManager.Model;
+
+namespace ppedv.PizzaOrderManager.Logic
+{
+    public class OrderService
+    {
+        public void PlaceOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> GetMyOrders(int addressId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetOrderStatus(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
