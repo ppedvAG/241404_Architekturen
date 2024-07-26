d14cbdf3914412b8b270c4267041e254a61717c3
André <AndreR@ppedv.de>
2024-07-26 10:04:49 +0200

    MVVM mit den Commuinity Toolkit

--- ppedv.PizzaOrderManager/ppedv.PizzaOrderManager.UI.WPF/App.xaml.cs
+++ ppedv.PizzaOrderManager/ppedv.PizzaOrderManager.UI.WPF/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using ppedv.PizzaOrderManager.Data.EfCore;
+using ppedv.PizzaOrderManager.Logic;
+using ppedv.PizzaOrderManager.Model.Contracts;
+using ppedv.PizzaOrderManager.UI.WPF.ViewModels;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +14,40 @@ namespace ppedv.PizzaOrderManager.UI.WPF
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Services = ConfigureServices();
+
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+        /// </summary>
+        public IServiceProvider Services { get; }
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            string conString = "Server=(localdb)\\mssqllocaldb;Database=PizzaOrderManager_test;Trusted_Connection=true;";
+
+            var services = new ServiceCollection();
+
+            services.AddTransient<IRepository>(x => new EfContextIRepositoryAdapter(conString));
+            services.AddSingleton<OrderService>();
+
+            services.AddSingleton<PizzaViewModel>();
+
+
+            return services.BuildServiceProvider();
+        }
     }
 
 }
--- ppedv.PizzaOrderManager/ppedv.PizzaOrderManager.UI.WPF/ViewModels/PizzaViewModel.cs
+++ ppedv.PizzaOrderManager/ppedv.PizzaOrderManager.UI.WPF/ViewModels/PizzaViewModel.cs
@@ -1,4 +1,7 @@
-﻿using ppedv.PizzaOrderManager.Model.DomainModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ppedv.PizzaOrderManager.Model.Contracts;
+using ppedv.PizzaOrderManager.Model.DomainModel;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
@@ -6,14 +9,13 @@ using System.Windows.Input;
 
 namespace ppedv.PizzaOrderManager.UI.WPF.ViewModels
 {
-    public class PizzaViewModel : INotifyPropertyChanged
+    public class PizzaViewModel : ObservableObject
     {
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
         public ObservableCollection<Pizza> PizzaList { get; set; } = new ObservableCollection<Pizza>();
-        
+
         private Pizza selectedPizza;
+        private readonly IRepository repo;
+
         public Pizza SelectedPizza
         {
             get => selectedPizza;
@@ -22,7 +24,9 @@ namespace ppedv.PizzaOrderManager.UI.WPF.ViewModels
                 selectedPizza = value;
                 //PropertyChanged.Invoke(this, new PropertyChangedEventArgs("SelectedPizza"));
                 //PropertyChanged.Invoke(this, new PropertyChangedEventArgs("PreisMitSteuer"));
-                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(""));
+                //PropertyChanged.Invoke(this, new PropertyChangedEventArgs(""));
+                OnPropertyChanged(nameof(SelectedPizza));
+                OnPropertyChanged(nameof(PreisMitSteuer));
             }
         }
 
@@ -32,21 +36,35 @@ namespace ppedv.PizzaOrderManager.UI.WPF.ViewModels
             {
                 if (SelectedPizza == null)
                     return "---";
-                
-                return (SelectedPizza.Price * 1.19m).ToString("c",new CultureInfo("de-DE")); 
+
+                return (SelectedPizza.Price * 1.19m).ToString("c", new CultureInfo("de-DE"));
             }
         }
 
         public AddNewCommand AddNewCommand { get; set; }
+        public ICommand NewAddNewCommand { get; set; }
+        public ICommand SaveCommand { get; set; }
 
-        public PizzaViewModel()
+        public PizzaViewModel(IRepository repo)
         {
-            PizzaList.Add(new Pizza() { Name = "Salami", Toppings = [new Topping() { Name = "Käse" }, new Topping() { Name = "Salami" }] });
-            PizzaList.Add(new Pizza() { Name = "Hawai", Toppings = [new Topping() { Name = "Käse" }, new Topping() { Name = "Ananas" }, new Topping() { Name = "Schinken" }] });
+            //PizzaList.Add(new Pizza() { Name = "Salami", Toppings = [new Topping() { Name = "Käse" }, new Topping() { Name = "Salami" }] });
+            //PizzaList.Add(new Pizza() { Name = "Hawai", Toppings = [new Topping() { Name = "Käse" }, new Topping() { Name = "Ananas" }, new Topping() { Name = "Schinken" }] });
 
             AddNewCommand = new AddNewCommand(PizzaList);
+            this.repo = repo;
+            PizzaList = new ObservableCollection<Pizza>(repo.GetAll<Pizza>());
+
+            NewAddNewCommand = new RelayCommand(UserWantsToAddANewPizza);
+            SaveCommand = new RelayCommand(() => repo.SaveChanges());
         }
 
+        private void UserWantsToAddANewPizza()
+        {
+            var newP = new Pizza() { Name = "NEU", Price = 7.77m };
+            repo.Add(newP);
+            PizzaList.Add(newP);
+            SelectedPizza = newP;
+        }
     }
 
     public class AddNewCommand : ICommand
